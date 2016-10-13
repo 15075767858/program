6be16c106b2dc19605c03c6ef342d889b11f5e9e
System Administrator <root@Mac.local>
2016-10-13 20:48:01 +0800

    提交

---
+++ build/temp/development/program/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Applications/MAMP/htdocs/htdocs/program/build/.sass-cache'
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+/Users/liuzhencai/bin/Sencha/Cmd/6.1.3.42/sencha app build
