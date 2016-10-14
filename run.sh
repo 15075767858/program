54de928b0eecc5304be2d60badaf2ba48c7938fb
System Administrator <root@Mac.local>
2016-10-14 14:25:12 +0800

    2.10

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+rm -rf resources/devsinfo
+rm -rf resources/devxml
+cp -R devsinfo/ resources/devsinfo
+cp -R devxml/ resources/devxml
 
 chmod -R 777 *
 /Users/liuzhencai/bin/Sencha/Cmd/6.1.3.42/sencha app build
 chmod -R 777 *
+cd build/production
+rm -rf modern
+rm -rf program/resources/font-awesome/
+rm -rf program/resources/fonts/
+rm -rf program/modern
+tar czvf program.tar.gz program
+
 #/Users/liuzhencai/bin/Sencha/Cmd/6.1.3.42/sencha app build testing
